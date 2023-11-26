581007f5ba740e6e614af6d1de2658519d82fbcb
MucahitKucuktepe <mehmet20132204@gmail.com>
2023-11-26 22:39:48 +0300

    ürünler dom a basıldı

--- app.js
+++ app.js
@@ -4,7 +4,16 @@ const searchInput = document.getElementById("searchInput");
 const categoryTitle = document.getElementById("category");
 
 const modalBody = document.querySelector(".modal-body");
-
+const btnColors = [
+  "primary",
+  "secondary",
+  "success",
+  "info",
+  "warning",
+  "danger",
+  "light",
+  "dark",
+];
 let products = [];
 let baskets = [];
 
@@ -16,9 +25,10 @@ const getProducts = async () => {
 
   products = data;
   category();
+  displayProducts(products);
 };
 getProducts();
-
+//!Kategorileri oluşturdugumuz fonksiyon
 const category = () => {
   console.log(products);
   // const categoryArr= products.map(item=>item.category)
@@ -32,6 +42,57 @@ const category = () => {
   // });
   // console.log(categoryArr);
 
-  //+ 2.yol
-  const categoryArr
+  // //? 2.yol
+  // const categoryArr=products.reduce((acc,item)=>{
+  //   if(!acc.includes(item.category)){
+  //     acc.push(item.category)
+  //   }
+  //   return acc
+  // },["All"])
+  // console.log(categoryArr);
+
+  //! 3.yol
+  const categoryArr = [
+    "All",
+    ...new Set(products.map((item) => item.category)),
+  ];
+  console.log(categoryArr);
+  categoryArr.forEach((category, i) => {
+    const btn = document.createElement("button");
+    btn.innerText = category.toUpperCase();
+    btn.classList.add("btn", `btn-${btnColors[i]}`);
+    btnDivs.appendChild(btn);
+  });
 };
+
+//!Ürünleri Dom a bastırma fonksiyonu
+
+function displayProducts(arr) {
+  productDivs.innerHTML = "";
+  arr.forEach((item) => {
+    const { id, title, description, price, image } = item;
+    const productDiv = document.createElement("div");
+    productDiv.classList.add("col");
+    productDiv.setAttribute("id",id);
+    productDiv.innerHTML = ` <div class="card">
+            <img src="${image}" class="p-2" height="250px" alt="...">
+            <div class="card-body">
+      <h5 class="card-title line-clamp-1">${title}</h5>
+              <p class="card-text line-clamp-3">${description}</p>
+            </div>
+            <div class="card-footer w-100 fw-bold d-flex justify-content-between gap-3">
+            <span>Price:</span><span>${price} $</span>
+                
+            </div>
+            <div class="card-footer w-100 d-flex justify-content-center gap-3">
+                <button class="btn btn-danger">
+                Sepete Ekle
+                </button>
+                <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                See Details
+                </button>
+            </div>
+          </div>`;
+          productDivs.appendChild(productDiv)
+  });
+}
