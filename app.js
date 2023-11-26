b0d42fa554165a678be7c2d7b9153c437f9cb572
MucahitKucuktepe <mehmet20132204@gmail.com>
2023-11-26 21:46:13 +0300

    category arr 1.way

--- app.js
+++ app.js
@@ -3,20 +3,35 @@ const productDivs = document.getElementById("products");
 const searchInput = document.getElementById("searchInput");
 const categoryTitle = document.getElementById("category");
 
-const modalBody=document.querySelector(".modal-body")
+const modalBody = document.querySelector(".modal-body");
 
 let products = [];
 let baskets = [];
 
-const getProducts = async ()=>{
-    const res = await fetch(
-      "https://anthonyfs.pythonanywhere.com/api/products/"
-    );
+const getProducts = async () => {
+  const res = await fetch("https://anthonyfs.pythonanywhere.com/api/products/");
 
-    const data = await res.json()
-    // console.log(data);
+  const data = await res.json();
+  // console.log(data);
 
-    products=data
-    console.log(products);
-}
-getProducts()
+  products = data;
+  category();
+};
+getProducts();
+
+const category = () => {
+  console.log(products);
+  // const categoryArr= products.map(item=>item.category)
+  // console.log(categoryArr);
+  //+ 1.yol
+  // let categoryArr = ["All"];
+  // products.forEach((item) => {
+  //   if (!categoryArr.includes(item.category)) {
+  //     categoryArr.push(item.category);
+  //   }
+  // });
+  // console.log(categoryArr);
+
+  //+ 2.yol
+  const categoryArr
+};
